2acf4fe37d700feffb6a02e76b434876b179aa8c
ra-lgt <raviajay9344@gmail.com>
2023-12-30 14:09:56 +0530

    service and about us page

--- Home.py
+++ Home.py
@@ -12,6 +12,17 @@ def Home():
 def doctor_profile():
     return render_template('doctor_profile.html')
 
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/service_details')
+def service_details():
+    return render_template('service_details.html')
+
+@app.route('/about')
+def about():
+    return render_template('about_us.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
