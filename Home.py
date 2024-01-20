ea29bf3eb4b9f99693ef5f46c9a2ff0033035813
ra-lgt <raviajay9344@gmail.com>
2024-01-20 19:40:07 +0530

    fixed thegoogle and email

--- Home.py
+++ Home.py
@@ -398,14 +398,14 @@ def mail_to_doctor(data,templates,subject):
         output_html = template.render(template_vars)
         message=MIMEMultipart('alternative')
         message['subject']=subject
-        message["from"]="skillstormofficial01@gmail.com"
+        message["from"]="zensyncsolutions@gmail.com"
         message["to"]=email
 
         html_mail=MIMEText(output_html,'html')
         message.attach(html_mail)
         server=smtplib.SMTP_SSL("smtp.gmail.com",465)
-        server.login("skillstormofficial01@gmail.com","wgrrwnsolhyfiyrg")
-        server.sendmail("skillstormofficial01@gmail.com",email,message.as_string())
+        server.login("zensyncsolutions@gmail.com","bdqyeshysfiuzksg")
+        server.sendmail("zensyncsolutions@gmail.com",email,message.as_string())
         return True
     except Exception as e:
         return False
